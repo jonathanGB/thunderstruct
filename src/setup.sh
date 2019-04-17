f0bf1f5ba966567fe667f78c96cbedfd553f4b99
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2019-04-17 23:33:58 +0000

    add setup script for ubuntu server

---
+++ src/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+sudo apt-get install ffmpeg
+sudo apt-get install python-scipy libsuitesparse-dev
+
+sudo apt-get install python3-pip
+sudo pip3 install numpy scipy scikit-sparse matplotlib IPython
