ac737710b107bdabd791cc25ab0f51e0bdaa6a48
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2019-04-21 15:41:39 -0400

    add go to setup

--- src/setup.sh
+++ src/setup.sh
@@ -8,3 +8,6 @@ sudo apt-get install python-scipy libsuitesparse-dev
 sudo apt-get install python3-pip
 sudo pip3 install numpy scipy matplotlib IPython
 sudo pip3 install scikit-sparse
+
+sudo snap install go --classic
+go build -o GoParallelizer.so -buildmode=c-shared GoParallelizer.go
