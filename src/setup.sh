f9b83418f3b383e7bd2904dd711fddb10941171c
Nick Pagel <pagel@Nicks-MacBook-Pro.local>
2019-05-07 22:40:08 -0400

    pyCuda documentation\

--- src/setup.sh
+++ src/setup.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get update
 
-sudo apt-get install ffmpeg
-sudo apt-get install python-scipy libsuitesparse-dev
+sudo apt-get install python3 python-dev python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip python-scipy libsuitesparse-dev ffmpeg
 
-sudo apt-get install python3-pip
-sudo pip3 install numpy scipy matplotlib IPython
+sudo pip3 install numpy scipy matplotlib IPython pymetis pycuda pyculib
 sudo pip3 install scikit-sparse
 
-sudo snap install go --classic
-go build -o GoParallelizer.so -buildmode=c-shared GoParallelizer.go
+export PGI=/opt/pgi;
+export PATH=/opt/pgi/linux86-64/18.10/bin:$PATH;
+export MANPATH=$MANPATH:/opt/pgi/linux86-64/18.10/man;
+export LM_LICENSE_FILE=$LM_LICENSE_FILE:/opt/pgi/license.dat;
